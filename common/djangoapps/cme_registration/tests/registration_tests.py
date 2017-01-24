73c31a795bcab45840a9fea15a8596f013b1b7a5
Nishant Karandikar <karandikar@stanford.edu>
2017-01-24 15:01:18 -0800

    Modify CME registration options & remove "other" option
    Changed two affiliation names. Removed option to select "other"
    as an affiliation, and removed validation for the free response field.
    Modified js in the cme_register.html file to
    omit scrolling animation that reveals a text field when "other" is
    selected as affiliation. Since users can no longer submit "other" as an affiliation with a
    free-response value, we remove validation for the other_affiliation
    post variable.

--- common/djangoapps/cme_registration/tests/registration_tests.py
+++ common/djangoapps/cme_registration/tests/registration_tests.py
@@ -53,7 +53,6 @@ class TestCmeRegistration(UrlResetMixin, TestCase):
                           'specialty': 'specialty',
                           'sub_specialty': 'sub_specialty',
                           'affiliation': 'affiliation',
-                          'other_affiliation': 'other_affiliation',
                           'sub_affiliation': 'sub_affiliation',
                           'sunet_id': 'sunet_id',
                           'stanford_department': 'stanford_department',
@@ -405,8 +404,7 @@ class TestCmeRegistration(UrlResetMixin, TestCase):
         self.post_vars['specialty_free'] = 'Patient care'
         self.post_vars['sub_specialty'] = 'Other'
         self.post_vars['sub_specialty_free'] = 'Legs and feet'
-        self.post_vars['affiliation'] = 'Other'
-        self.post_vars['other_affiliation'] = 'other_affiliation'
+        self.post_vars['affiliation'] = 'Not affiliated with Stanford Medicine'
         self.post_vars['sub_affiliation'] = ''
 
         url = reverse('create_account')
@@ -436,8 +434,7 @@ class TestCmeRegistration(UrlResetMixin, TestCase):
                                                          license_state='license_state',
                                                          physician_status='physician_status',
                                                          patient_population='patient_population',
-                                                         affiliation='Other',
-                                                         other_affiliation='other_affiliation',
+                                                         affiliation='Not affiliated with Stanford Medicine',
                                                          sub_affiliation='',
                                                          sunet_id='sunet_id',
                                                          stanford_department='stanford_department',
